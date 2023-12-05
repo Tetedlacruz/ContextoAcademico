ff2866f5bfc0b68bdf19aab5d84119adabe1aac0
Fialho <Fialho@FIALHO>
2023-12-05 00:51:33 -0300

    Adicionar arquivos de projeto.

---
+++ ContextoAcademico/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoAcademico
+{
+    internal class Model
+    {
+        //Tabela Aluno
+        public class Aluno
+        {
+            public int AlunoId { get; set; }
+            public string Nome { get; set; }
+            public ICollection<Disciplina> Disciplinas { get; set; }
+        }
+
+        //Tabela Disciplina
+        public class Disciplina
+        {
+            public int DisciplinaId { get; set; }
+            public string Nome { get; set; }
+            public ICollection<Aluno> Alunos { get; set; }
+        }
+
+        //Tabela de junção AlunoDisciplina
+        public class AlunoDisciplina
+        {
+            public int AlunoId { get; set; }
+            public int DisciplinaId { get; set; }
+        }
+    }
+}
---
+++ ContextoAcademico/Program.cs
@@ -0,0 +1,14 @@
+﻿using static ContextoAcademico.Model;
+
+Aluno aluno = new();
+Disciplina disciplina = new();
+
+//Cadastrando o Aluno e a Disciplina
+aluno.Nome = "Maria";
+disciplina.Nome = "Matemática";
+
+// Matriculando um aluno em uma disciplina
+aluno.Disciplinas.Add(disciplina);
+
+// Associando uma disciplina a um aluno
+disciplina.Alunos.Add(aluno);
